03e49c54c4ca8190fddfe438b6400e8d2f70c4c0
user <user@users-MacBook-Pro-3.local>
2025-03-05 19:29:27 +0300

    changes to procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 manage.py runserver 0.0.0.0:
+web: python3 ryan/manage.py runserver 0.0.0.0:8080
--- ryan/ryan/settings.py
+++ ryan/ryan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lz_@=2$u%hav)=!%s$s=4kfogq0z%t*z+wctlmthru0y7)5!zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
